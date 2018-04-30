bb684a41eec5fd456c024867e2c626391b6672e8
GelGel <omargalal20084@yahoo.com>
2018-04-30 09:57:13 +0200

    Changed Form looks

---
+++ UniAdvise/MainWindow.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniAdvise {
+    public partial class MainWindow : Form {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void bunifuImageButton1_Click_1(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void header_Paint(object sender, PaintEventArgs e) {
+
+        }
+
+        private void bunifuImageButton4_Click(object sender, EventArgs e) {
+            this.WindowState = FormWindowState.Minimized;
+
+        }
+
+        private void bunifuImageButton3_Click(object sender, EventArgs e) {
+            if (WindowState.ToString() == "Normal") {
+                this.WindowState = FormWindowState.Maximized;
+            } else {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void createAdmin1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void minimize_Click(object sender, EventArgs e) {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void createAdmin_Load(object sender, EventArgs e) {
+
+        }
+
+
+        private void tile1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e) {
+
+        }
+
+        private void createAdmin_Load_1(object sender, EventArgs e) {
+
+        }
+
+    }
+}
